f2bb47fba269f537f1d3b913261497091590004e
Яна Цховребова <yaczxovrebova@belt-app.com>
2024-12-19 17:01:20 +0300

    test2 rk3

--- test/test_02/main.py
+++ test/test_02/main.py
@@ -1,3 +1,5 @@
+# ЗАДАНИЕ 2
+
 from peewee import *
 import datetime
 
@@ -15,16 +17,49 @@ db = PostgresqlDatabase(
     port=DB_PORT
 )
 
+# найти все страны, в которых меньше 55 космических аппаратов
 Q1 = '''
-select 1;
+select country
+from satellite
+group by country
+having count(*) < 55;
 '''
 
+# найти КА, которые в общей сложности пробыли в космосе более 40 дней
 Q2 = '''
-select 1;
+select
+    s.name,
+    s.country,
+    sum(f2.lnch_date - f1.lnch_date) as days_in_space
+from flight f1
+left join flight f2
+on  f1.sid = f2.sid 
+    and f1.type = 1 
+    and f2.type = 0
+    and f2.lnch_date > f1.lnch_date
+    and not exists (
+        select 1
+        from flight f3
+        where f3.sid = f1.sid
+            and f3.type = 1
+            and f3.lnch_date > f1.lnch_date
+            and f3.lnch_date < f2.lnch_date
+    )
+join satellite s
+on f1.sid = s.id
+group by s.id, s.name, s.country
+having sum(f2.lnch_date - f1.lnch_date) > 40;
 '''
 
+# найти КА, который в прошлом году вернулся последним
 Q3 = '''
-select 1;
+select s.*
+from satellite s
+join flight f on s.id = f.sid
+where f.type = 0 
+    and extract(year from f.lnch_date) = extract(year from current_date) - 1
+order by f.lnch_date, f.lnch_time desc
+limit 1;
 '''
 
 class BaseModel(Model):
@@ -48,42 +83,99 @@ def q1_sql():
     result = db.execute_sql(Q1)
     for row in result:
         print(row)
-    print('\n')
 
 def q2_sql():
     result = db.execute_sql(Q2)
     for row in result:
         print(row)
-    print('\n')
 
 def q3_sql():
     result = db.execute_sql(Q3)
     for row in result:
         print(row)
-    print('\n')
 
 def q1_orm():
-    pass
+    query = (
+        Satellite
+        .select(Satellite.country)
+        .group_by(Satellite.country)
+        .having(fn.COUNT(Satellite.id) < 55)
+    )
+    for q in query:
+        print(f'{q.country}')
 
 def q2_orm():
-    pass
+    f1 = Flight.alias('f1')
+    f2 = Flight.alias('f2')
+    query = (
+        Satellite
+        .select(
+            Satellite.name,
+            Satellite.country,
+            fn.SUM(f2.lnch_date - f1.lnch_date).alias('days_in_space')
+        )
+        .join(f1, on=(Satellite.id == f1.sid))
+        .join(
+            f2,
+            on=(
+                (f1.sid == f2.sid) &
+                (f1.type == 1) &
+                (f2.type == 0) &
+                (f2.lnch_date > f1.lnch_date) &
+                ~fn.EXISTS(
+                    Flight
+                    .select(1)
+                    .where(
+                        (f1.sid == Flight.sid) &
+                        (Flight.type == 1) &
+                        (Flight.lnch_date > f1.lnch_date) &
+                        (Flight.lnch_date < f2.lnch_date)
+                    )
+                )
+            )
+        )
+        .group_by(Satellite.id, Satellite.name, Satellite.country)
+        .having(fn.SUM(f2.lnch_date - f1.lnch_date) > 40)
+    )
+    for satellite in query:
+        print(f"{satellite.name}, {satellite.country}, {satellite.days_in_space}")
 
 def q3_orm():
-    pass
+    query = (
+        Satellite
+        .select(Satellite, Flight.lnch_date)
+        .join(Flight, on=(Satellite.id == Flight.sid))
+        .where(
+            (Flight.type == 0),
+            (SQL(f'extract (year from lnch_date) = {datetime.datetime.now().year - 1}'))
+        )
+        .order_by(Flight.lnch_date.desc())
+        .order_by(Flight.lnch_time.desc())
+        .limit(1)
+    )
+    result = query.first()
+    if result:
+        print(f'{result.id}, {result.name}, {result.prod_date}, {result.country}')
 
 if __name__ == '__main__':
     db.connect()
 
-    print('--- Q1 ---')
+    print('-------- Q1 --------')
+    print('-- SQL --')
     q1_sql()
+    print('-- ORM --')
     q1_orm()
 
-    print('--- Q2 ---')
+    print('-------- Q2 --------')
+    print('-- SQL --')
     q2_sql()
+    print('-- ORM --')
     q2_orm()
 
-    print('--- Q3 ---')
+    print('-------- Q3 --------')
+    print('-- SQL --')
     q3_sql()
+    print('-- ORM --')
     q3_orm()
 
     db.close()
--- test/test_02/main.sql
+++ test/test_02/main.sql
@@ -1,9 +1,10 @@
+-- ЗАДАНИЕ 1
+
 drop database if exists rk3;
 create database rk3;
 \c rk3;
-
-drop table if exists satellite;
 drop table if exists flight;
+drop table if exists satellite;
 
 create table satellite (
     id serial primary key,
@@ -39,7 +40,6 @@ insert into satellite (
 ) values
     ('SJT-2086', '2050-01-01', 'Россия'),
     ('Шинцзян 16-02', '2049-12-01', 'Китай');
-
 insert into flight (
     sid, 
     lnch_date, 
@@ -50,5 +50,27 @@ insert into flight (
     (1, '2050-05-11', '09:00:00', 'Среда', 1),
     (1, '2051-06-11', '23:05:00', 'Среда', 0),
     (1, '2051-10-10', '23:50:00', 'Вторник', 1),
-    (2, '2024-05-11', '15:15:00', 'Среда', 1),
-    (1, '2052-01-01', '12:15:00', 'Понедельник', 0);
+    (2, '2052-05-11', '15:15:00', 'Среда', 1),
+    (1, '2052-01-01', '12:15:00', 'Понедельник', 0),
+    (2, '2052-07-11', '15:15:00', 'Среда', 0),
+    (2, '2023-01-01', '12:15:00', 'Понедельник', 1),
+    (2, '2023-12-31', '23:59:00', 'Понедельник', 0),
+    (1, '2023-05-11', '23:50:00', 'Вторник', 1),
+    (1, '2023-12-31', '23:55:00', 'Вторник', 0);
+    
+-- 1 - получить КА, которые не участвовали в полётах (нет информации о полетах)
+explain select 
+    s.id, 
+    s.name
+from satellite s
+where not exists (
+    select 1
+    from flight f
+   where s.id = f.sid
+);
+
+-- 2 - получить идентификаторы КА, которые отправлялись или прилетали более 5 раз
+explain select sid
+from flight
+group by sid
+having count(*) > 5;
